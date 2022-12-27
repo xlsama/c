b72d4e2e9ae337ae2e21343661db6b9eecfbb9d0
xlsama <im.xlsama@gmail.com>
2022-12-27 12:22:08 +0800

    chore: update

--- 9/9-1.cpp
+++ 9/9-1.cpp
@@ -1,14 +1,46 @@
+#include <cstdio>
 #include <stdio.h>
 
-#define MaxSize 50;
+#define MaxSize 50
 typedef int ElemType;
 typedef struct {
   ElemType data[MaxSize];
   int len;
 } SqList;
 
+bool list_insert(SqList &L, int pos, ElemType element) {
+  if (pos < 1 || pos > L.len + 1 || L.len >= MaxSize) {
+    return false;
+  }
+  for (int i = L.len; i >= pos; i--) {
+    L.data[i] = L.data[i - 1];
+  }
+  L.data[pos - 1] = element;
+  L.len++;
+  return true;
+}
+
+void print_list(SqList L) {
+  for (int i = 0; i < L.len; i++) {
+    printf("%4d", L.data[i]);
+  }
+  printf("\n");
+}
+
 int main() {
-  SqList;
+  SqList L;
+  L.data[0] = 1;
+  L.data[1] = 2;
+  L.data[2] = 3;
+  L.len = 3;
+  bool result = list_insert(L, 2, 60);
+  if (result) {
+    printf("insert success\n");
+    print_list(L);
+
+  } else {
+    printf("insert failed\n");
+  }
 
   return 0;
 }
